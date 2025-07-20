e56747e848379520f562d863f2e86c7b4ed5dc85
Tobias Zethoven <tzethoven@gmail.com>
2025-07-20 13:55:32 +0200

    add env variables to build script

--- pi-setup.sh
+++ pi-setup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+# Load environment variables if .env exists
+if [ -f ".env" ]; then
+    set -a
+    source .env
+    set +a
+fi
+
 echo "Running npm install..."
 npm install
 
