a97b0fe91f74a66f0d383e797763197fbe94ac6d
Tobias Zethoven <tzethoven@gmail.com>
2025-07-21 14:16:45 +0200

    update session duration

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -5,6 +5,10 @@ export const auth = betterAuth({
 	database: db,
 	emailAndPassword: {
 		enabled: true
+	},
+	session: {
+		expiresIn: 60 * 60 * 24 * 7, // 7 days
+		updateAge: 60 * 60 * 24 // 1 day (every 1 day the session expiration is updated)
 	}
 });
 
